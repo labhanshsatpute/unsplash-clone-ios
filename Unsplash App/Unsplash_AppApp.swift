dd23565a0694ed64d48e9a8a53b242ca7418ccf3
Labhansh Satpute <73733885+labhanshsatpute@users.noreply.github.com>
2023-11-12 10:40:07 +0530

    Initial Commit

---
+++ Unsplash App/Unsplash_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unsplash_AppApp.swift
+//  Unsplash App
+//
+//  Created by Labhansh Satpute on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Unsplash_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
