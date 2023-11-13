5b93a119f656ec0d0478bd228aeb88b83975bf78
Labhansh Satpute <73733885+labhanshsatpute@users.noreply.github.com>
2023-11-13 23:33:55 +0530

    minor changes & bug fixes

--- Unsplash App/Components/InputBox.swift
+++ Unsplash App/Components/InputBox.swift
@@ -11,9 +11,13 @@ struct InputBoxWithButton: View {
     
     @Binding var text: String
     var placeHolder: String
+    var keyUp: () -> Void
     
     var body: some View {
-        TextField(placeHolder, text: $text)
+        TextField(placeHolder, text: $text, onEditingChanged: { isEditing in if !isEditing {
+            self.keyUp()
+            }
+        })
             .padding(.horizontal, 15)
             .padding(.vertical, 13)
             .background(Color.gray.opacity(0.1))
